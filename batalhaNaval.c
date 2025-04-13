3bc1cd8e4f40364b15bf56a0d016f9e64525b2b7
alpha341 <141239314+alpha341@users.noreply.github.com>
2025-04-13 01:57:44 +0000

    VERSÃO 2 - CONE, CRUZ E OCTAEDRO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -17,68 +17,139 @@ int main() {
                               {0,0,0,0,0,0,0,0,0,0},};
     int navio1 [3] = {3,3,3};
     int navio2 [3] = {3,3,3};
+    int escolha, coneLinha, coneColuna, cruzLinha, cruzColuna, octaLinha, octaColuna;
+    
+    
+
+    //Para escolherem
+    printf("Qual opção você deseja?\n");
+    printf("1 - Cone\n");
+    printf("2 - Cruz\n");
+    printf("3 - Octaedro\n");
+    scanf("%i", &escolha);
 
 
-    for (int i=0; i<10 ;i++){
-        for (int j = 0; j<10; j++){
-            if (i==3 && j==2){
-                tabuleiro [i][j] = navio1[0];
-                tabuleiro [i+1][j] = navio1[1];
-                tabuleiro [i+2][j] = navio1[2];
-            }
+
+    switch (escolha)
+    {
+    case 1:
+    //Para o cone
+    printf("\nPara o cone. Insira o valor da primeira linha(de 1 a 10):\n");
+    scanf("%i",&coneLinha);
+    coneLinha = coneLinha-1;
+    printf("Para o cone. Insira o valor da última coluna (maior ou igual a 5 e até 10):\n");
+    scanf("%i",&coneColuna);
+    coneColuna = coneColuna-1;
+    //Caso não seja possível colocar a base nos limites do tabuleiro
+    if (coneColuna<4){
+        printf("O cone vai extrapolar os limites do tabuleiro. Tabuleiro estará todo zerado.\n");
+        break;
+    }
+    //Faz o cone de acordo com a escolha do usuário
+    for (int i = coneLinha; i<coneLinha+3; i++){
+        if (i==coneLinha){
+            tabuleiro[i][coneColuna-2] = 1;
+        }
+        if (i==coneLinha+1){
+            tabuleiro[i][coneColuna-1] = 1;
+            tabuleiro[i][coneColuna-2] = 1;
+            tabuleiro[i][coneColuna-3] = 1;
+        }
+        if (i==coneLinha+2){
+            tabuleiro[i][coneColuna] = 1;
+            tabuleiro[i][coneColuna-1] = 1;
+            tabuleiro[i][coneColuna-2] = 1;
+            tabuleiro[i][coneColuna-3] = 1;
+            tabuleiro[i][coneColuna-4] = 1;
         }
     }
+    break;
 
-
-    for (int i=0; i<10 ;i++){
-        for (int j = 0; j<10; j++){
-            if (i==7 && j==5){
-                tabuleiro [i][j] = navio1[0];
-                tabuleiro [i][j+1] = navio1[1];
-                tabuleiro [i][j+2] = navio1[2];
-            }
+    case 2:
+     //Para a cruz
+     printf("\nPara a cruz. Insira o valor da primeira linha(de 1 a 10):\n");
+     scanf("%i",&cruzLinha);
+     cruzLinha = cruzLinha-1;
+     printf("Para a cruz. Insira o valor da coluna do meio (maior ou igual a 5 e até 10):\n");
+     scanf("%i",&cruzColuna);
+     //Caso não seja possível colocar a base nos limites do tabuleiro
+     cruzColuna = cruzColuna-1;
+     if (cruzColuna<4){
+         printf("A cruz vai extrapolar os limites do tabuleiro. Tabuleiro estará todo zerado.\n");
+         break;
+     }
+     //Desenha a cruz no tabuleiro - Aparece com número 2
+     for (int i = cruzLinha; i<cruzLinha+3; i++){
+        if (i==cruzLinha){
+            tabuleiro[i][cruzColuna-2] = 2;
         }
+        if (i==cruzLinha+1){
+            tabuleiro[i][cruzColuna] = 2;
+            tabuleiro[i][cruzColuna-1] = 2;
+            tabuleiro[i][cruzColuna-2] = 2;
+            tabuleiro[i][cruzColuna-3] = 2;
+            tabuleiro[i][cruzColuna-4] = 2;
+        }
+        if (i==cruzLinha+2){
+            tabuleiro[i][cruzColuna-2] = 2;
+        }
+     }
+    break;
+
+    case 3:
+     //Para o octaedro
+     printf("\nPara o octaedro. Insira o valor da primeira linha (de 1 a 10):\n");
+     scanf("%i",&octaLinha);
+     octaLinha = octaLinha -1;
+     printf("Para o octaedro. Insira o valor da coluna do meio (maior ou igual a 3 e até 10):\n");
+     scanf("%i",&octaColuna);
+     octaColuna = octaColuna-1;
+     //Caso não seja possível colocar a base nos limites do tabuleiro
+     if (octaColuna<2){
+         printf("O octaedro vai extrapolar os limites do tabuleiro. Tabuleiro estará todo zerado.\n");
+         break;
+     }
+     //Desenha o octaedro no tabuleiro - Aparece com número 4
+     for (int i = octaLinha; i<octaLinha+3; i++){
+        if (i==octaLinha){
+            tabuleiro[i][octaColuna-1] = 4;
+        }
+        if (i==octaLinha+1){
+            tabuleiro[i][octaColuna] = 4;
+            tabuleiro[i][octaColuna-1] = 4;
+            tabuleiro[i][octaColuna-2] = 4;
+        }
+        if (i==octaLinha+2){
+            tabuleiro[i][octaColuna-1] = 4;
+        }
+     }
+    break;
+  
+    default:
+        printf("Você escolheu opção inválida.");
+        break;
+    }      
+    
+    
+   
+    // Espaço para alinhar com os números da lateral
+    printf("\t");
+      for (char letra = 'A'; letra <= 'J'; letra++) {
+        printf("%c\t", letra);
     }
+    printf("\n");
 
 
     for (int k = 0; k<10; k++){
-        for (int l = 0; l<10; l++){
-            printf("%i\t",tabuleiro[k][l]);
-        }
+        printf("%i\t",k+1);
+            for (int l = 0; l<10; l++){
+                printf("%i\t",tabuleiro[k][l]);
+            }
         printf("\n");
+        if (k ==9){
+            printf("\n\n");
+        }
     }
-
-
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
